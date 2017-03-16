5be84b603e00813382697ef2aeeb1cec3a5d368b
alexy <od-5@yandex.ru>
2017-03-16 20:25:44 +0300

    fix

--- apps/landing/urls.py
+++ apps/landing/urls.py
@@ -8,5 +8,6 @@ __author__ = 'alexy'
 urlpatterns = patterns(
     'apps.landing.views',
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
+    url(r'^confidentiality/$', TemplateView.as_view(template_name='confidentiality.html'), name='confidentiality'),
     url(r'^thnx/$', TemplateView.as_view(template_name='ok.html'), name='thnx'),
 )
