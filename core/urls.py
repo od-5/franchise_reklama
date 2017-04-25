9b055b6b56c85f9b0e60d643aef90202206d3970
alexy <od-5@yandex.ru>
2017-04-25 11:35:19 +0300

    fix

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,13 @@
 # coding=utf-8
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 
 __author__ = 'alexy'
 
 
 urlpatterns = patterns(
     'core.views',
+    url(r'^googlecadc02ecd2bbe3fb\.html', TemplateView.as_view(template_name='googlecadc02ecd2bbe3fb.html')),
     url(r'^robots\.txt', 'get_robots_txt', name='robots'),
     url(r'^sitemap\.xml', 'get_sitemap_xml', name='sitemap'),
 )
